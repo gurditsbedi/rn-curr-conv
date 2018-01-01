d50d729b3afb2d6a9e05d70043dd8a1e1463b4c7
Gurdit Singh Bedi <gurditsbedi@gmail.com>
2018-01-01 23:52:44 +0530

    Added Themes Screen Modified ListItem and Icon in List Component accordingly

--- app/index.js
+++ app/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Home from './screens/Home';
-import CurrencyList from './screens/CurrencyList';
-import Options from './screens/Options';
+import Themes from './screens/Themes';
 
 EStyleSheet.build({
   $primaryBlue: '#4f6d7a',
+  $primaryOrange: '#d57a66',
+  $primaryGreen: '#00bd9d',
+  $primaryPurple: '#9e768f',
+
   $white: '#fff',
-  $lightGray: '#F0F0F0',
-  $border: '#E2E2E2',
+  $lightGray: '#f0f0f0',
+  $border: '#e2e2e2',
   $inputText: '#797979',
   $darkText: '#343434',
 
   // $outline: 1,
 });
 
-export default () => <Options />;
+export default () => <Themes />;
